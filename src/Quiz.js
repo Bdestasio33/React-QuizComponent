464775d686b7006254942a4fcedaacc116e8c7fe
bdestasio33 <bdestasio33@gmail.com>
2019-11-13 09:52:11 -0500

    Completed module

--- src/Quiz.js
+++ src/Quiz.js
@@ -14,8 +14,9 @@ class Quiz extends Component {
       return { quiz_position: state.quiz_position + 1 };
     });
   }
+
   handleResetClick() {
-    this.props.resetClickHandler();
+    this.setState({ quiz_position: 1 });
   }
 
   render() {
--- src/QuizEnd.js
+++ src/QuizEnd.js
@@ -2,8 +2,9 @@ import React, { Component } from "react";
 
 class QuizEnd extends Component {
   handleResetClick() {
-    this.setState({ quiz_position: 1 });
+    this.props.resetClickHandler();
   }
+
   render() {
     return (
       <div>
